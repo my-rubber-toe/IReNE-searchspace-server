a05c74bde101b8f59bc4c691cb6095cedf84d5a1
Jainel M. Torres Santos <jainel.torres@upr.edu>
2020-03-24 20:43:22 -0400

    fix(DAOs): added one DAO

--- DAOs/dao_SS.py
+++ DAOs/dao_SS.py
@@ -2,9 +2,14 @@ from mongoengine import *
 from schema_DB import *
 import json
 
-def get_doc_collab(collabid):
-    get_docs = Document.objects.filter(creatoriD= collabid)
-    return json.loads(get_docs.to_json())
+def post_access_request(**ac_req):
+    new_collab = Collaborator(first_name = ac_req["first_name"], last_name = ac_req["last_name"],
+    email = ac_req["email"], faculty = ac_req["faculty"])
+    new_collab.save()
+
+def get_docs():
+    get_doc = DocumentCase.objects()
+    return json.loads(get_doc.to_json())
 
 def get_doc(docid):
     get_doc = DocumentCase.objects.get(id = docid)
@@ -20,4 +25,4 @@ def get_doc_infrastructure_type(infras):
 
 def get_doc_tag_type(tag):
     get_docs = DocumentCase.objects.filter(tagsDoc__contains = tag)
-    return json.loads(get_docs.to_json())   
+    return json.loads(get_docs.to_json()) 
