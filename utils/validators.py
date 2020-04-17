f3b234f557ab40596d9a1a6dfab8f0f55839012b
Alejandro Vasquez <alejandro.vasquez@upr.edu>
2020-04-17 11:56:04 -0400

    fix(validators): commented unused validators
    Commented but not delete unused validators for the case that they become useful

--- utils/validators.py
+++ utils/validators.py
@@ -1,78 +1,93 @@
 from marshmallow import validate, Schema, fields
 
+# unused
+# class Authors(Schema):
+#     """Nested Schema"""
+#     first_name = fields.String(required=False, validate=validate.Length(min=1))
+#     last_name = fields.String(required=False, validate=validate.Length(min=1))
+#     email = fields.Email(
+#         required=False,
+#         validate=validate.Regexp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")
+#     )
+#     faculty = fields.String(required=False, validate=validate.Length(min=1))
+#
+#
+# class Actors(Schema):
+#     """Nested Schema"""
+#     first_name = fields.String(required=False, validate=validate.Length(min=1))
+#     last_name = fields.String(required=False, validate=validate.Length(min=1))
+#     role = fields.String(required=False, validate=validate.Length(min=1))
+#
+#
+# class General(Schema):
+#     """ Nested Schema"""
+#     authors = fields.List(fields.Nested(Authors), required=False, validate=validate.Length(1))
+#     actors = fields.List(fields.Nested(Actors), required=False, validate=validate.Length(1))
+#     description = fields.String(required=False, validate=validate.Length(min=1))
+#     title = fields.String(required=False, validate=validate.Length(min=1))
+#
 
-class Authors(Schema):
-    """Nested Schema"""
-    first_name = fields.String(required=False, validate=validate.Length(min=1))
-    last_name = fields.String(required=False, validate=validate.Length(min=1))
-    email = fields.Email(
-        required=False,
-        validate=validate.Regexp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")
-    )
-    faculty = fields.String(required=False, validate=validate.Length(min=1))
+# unused
+# class GetDocumentsValidator(Schema):
+#     """ Request body schema for the endpoint /api/documents"""
+#     general = fields.List(fields.Nested(General, required=False), required=False)
+#
+#     publication_date = fields.List(
+#         fields.Date(),
+#         required=False,
+#         allow_none=False,
+#         validate=validate.Length(min=1)
+#     )
+#
+#     tags = fields.List(
+#         fields.String(),
+#         required=False,
+#         validate=validate.Length(min=1)
+#     )
+#
+#     incident_dates = fields.List(
+#         fields.Date(),
+#         required=False,
+#         validate=validate.Length(min=1))
+#
+#     infrastructure_type = fields.List(
+#         fields.String(),
+#         required=False,
+#         validate=validate.Length(min=1))
+#
+#     damage_type = fields.List(
+#         fields.String(),
+#         required=False,
+#         validate=validate.Length(min=1))
+#
+#     language = fields.String(
+#         required=False,
+#         default='English',
+#         validate=validate.Length(min=1)
+#     )
 
-
-class Actors(Schema):
-    """Nested Schema"""
-    first_name = fields.String(required=False, validate=validate.Length(min=1))
-    last_name = fields.String(required=False, validate=validate.Length(min=1))
-    role = fields.String(required=False, validate=validate.Length(min=1))
-
-
-class General(Schema):
-    """ Nested Schema"""
-    authors = fields.List(fields.Nested(Authors), required=False, validate=validate.Length(1))
-    actors = fields.List(fields.Nested(Actors), required=False, validate=validate.Length(1))
-    description = fields.String(required=False, validate=validate.Length(min=1))
-    title = fields.String(required=False, validate=validate.Length(min=1))
-
-
-class GetDocumentsValidator(Schema):
-    """ Request body schema for the endpoint /api/documents"""
-    general = fields.List(fields.Nested(General, required=False), required=False)
-
-    publication_date = fields.List(
-        fields.Date(),
-        required=False,
-        allow_none=False,
-        validate=validate.Length(min=1)
-    )
-
-    tags = fields.List(
-        fields.String(),
-        required=False,
-        validate=validate.Length(min=1)
-    )
-
-    incident_dates = fields.List(
-        fields.Date(),
-        required=False,
-        validate=validate.Length(min=1))
-
-    infrastructure_type = fields.List(
-        fields.String(),
-        required=False,
-        validate=validate.Length(min=1))
-
-    damage_type = fields.List(
-        fields.String(),
-        required=False,
-        validate=validate.Length(min=1))
-
-    language = fields.String(
-        required=False,
-        default='English',
-        validate=validate.Length(min=1)
-    )
-
-
-class GetDocIdValidator(Schema):
-    """ Request body schema for the endpoint /api/documents/view/<doc_id>/"""
-    doc_id = fields.String(required=True, validate=validate.Length(equal=24))
+#
+# class GetDocIdValidator(Schema):
+#     """ Request body schema for the endpoint /api/documents/view/<doc_id>/"""
+#     doc_id = fields.String(required=True, validate=validate.Length(equal=24))
+#
 
 
 class GetCollaboratorRequestValidator(Schema):
-    """ Request body schema for the endpoint /api/collabrequest/"""
+    """
+
+    Request body schema for the endpoint /api/collabrequest/
+
+    Parameters
+    ----------
+    Schema
+        A JSON with the fields first_name, last_name and email
+
+    Raises
+    ------
+    Validation Error
+        If one of the fields is not valid
+    """
     first_name = fields.String(required=True, validate=validate.Length(min=1, max=30))
     last_name = fields.String(required=True, validate=[
         validate.Length(min=1, max=30)])
@@ -85,10 +100,10 @@ class GetCollaboratorRequestValidator(Schema):
     )
     faculty = fields.String(required=False, validate=validate.Length(min=1))
 
-
-class GetComparisonValidator(Schema):
-    """ Request body schema for the endpoint /api/visualize/comparison-graph"""
-    xcat = fields.String(required=True, validate=validate.Length(min=1))
-    ycat = fields.String(required=True, validate=validate.Length(min=1))
-    xvalue = fields.String(required=True, validate=validate.Length(min=1))
-    yvalue = fields.String(required=True, validate=validate.Length(min=1))
+# unused
+# class GetComparisonValidator(Schema):
+#     """ Request body schema for the endpoint /api/visualize/comparison-graph"""
+#     xcat = fields.String(required=True, validate=validate.Length(min=1))
+#     ycat = fields.String(required=True, validate=validate.Length(min=1))
+#     xvalue = fields.String(required=True, validate=validate.Length(min=1))
+#     yvalue = fields.String(required=True, validate=validate.Length(min=1))
